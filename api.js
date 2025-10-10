9756e402f8ca00e45191f078db9aba1798b664a8
Rincon23 <enzorincon2003@gmail.com>
2025-10-10 16:02:16 -0300

    Instalando e autorizando âmbiente CORS; Mudei a senha para string para testar na pagina HTML

--- api.js
+++ api.js
@@ -1,9 +1,14 @@
 import express from "express";
+import cors from "cors";
 import router from "./routes/authRoutes.js";
 import testRouter from "./routes/testRoutes.js";
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors({
+  origin: ["http://127.0.0.1:5500", "http://localhost:8080"]
+}));
 
 app.use(express.json());
 app.use(router);
--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
 export const users = [
-    { id: 1, username: 'Enzo', password: 12345678, role: 'admin' },
-    { id: 2, username: 'Peterson', password: 12345678, role: 'user' }
+    { id: 1, username: 'Enzo', password: '12345678', role: 'admin' },
+    { id: 2, username: 'Peterson', password: '12345678', role: 'user' }
 ];
