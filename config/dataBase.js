f5acaa4275bff9da647cb7c66ebabd0a48056f18
Rincon23 <enzorincon2003@gmail.com>
2025-10-14 10:09:32 -0300

    Configurando docker:
    Criei uma imagem publica para que possa rodar em qualquer ambiente.
    
    3 imagens foram criadas:
    Api;
    MySQL; e
    phpadmin (para visualizar o MySQL)
    
    Criei um github-action para que todas as veses que eu der um git-push o repositório do docker também dará um docker-push

--- config/dataBase.js
+++ config/dataBase.js
@@ -1,8 +1,14 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("framework2", "root", "", {
-  host: "localhost",
+const DB_NAME = process.env.DB_NAME || "framework2";
+const DB_USER = process.env.DB_USER || "root";
+const DB_PASS = process.env.DB_PASS || "";
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_PORT = Number(process.env.DB_PORT || 3306);
+
+export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+  host: DB_HOST,
   dialect: "mysql",
-  port: 3306,
+  port: DB_PORT,
   logging: false,
 });
