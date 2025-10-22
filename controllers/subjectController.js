82619dde7fbc3b30219d55ba7d7ad54dcba7dda7
Rincon23 <enzorincon2003@gmail.com>
2025-10-22 14:25:09 -0300

    Adicionando delete e patch para a tabela subject

--- controllers/subjectController.js
+++ controllers/subjectController.js
@@ -13,8 +13,8 @@ export const addSubject = async (req, res) => {
     res.status(201).json(addSubject);
     
   } catch (error) {
-  console.error("❌ Erro ao criar usuário:", error);
-  res.status(500).json({ message: "Erro ao criar usuário", error: error.message });
+  console.error("❌ Erro ao criar disciplina:", error);
+  res.status(500).json({ message: "Erro ao criar disciplina", error: error.message });
 }
 
 };
@@ -25,7 +25,51 @@ export const listSubject = async (req, res) => {
       res.status(200).json(subjects);
 
   } catch (error) {
-      console.error("❌ Erro ao criar usuário:", error);
-      res.status(500).json({ message: "Erro ao criar usuário", error: error.message });
+      console.error("❌ Erro ao listar disciplina:", error);
+      res.status(500).json({ message: "Erro ao listar disciplina:", error: error.message });
   }
 };
+
+export const deleteSubject = async (req, res) => {
+  try {
+    const {name} = req.body;
+    const existingSubject = await Subject.findOne({ where: { name } });
+    if (!existingSubject) {
+      return res.status(404).json({ message: "Disciplina não existe" });
+    }
+
+    await Subject.destroy({ where: { name } });
+    res.status(200).json({ message: "Disciplina foi removida!" });
+  } catch (error) {
+      console.error("❌ Erro ao deletar disciplina:", error);
+      res.status(500).json({ message: "Erro ao criar disciplina", error: error.message });
+  }
+}
+
+export const patchSubject = async (req, res) => {
+  try {
+    const {oldName, newName} = req.body;
+
+    //Verifica se existe nome 
+    const existingSubject = await Subject.findOne({ where: { name: oldName } });
+    if (!existingSubject) {
+      return res.status(404).json({ message: "Disciplina não existe" });
+    }
+
+    //Verifica se o novo nome já está em uso
+    const inUse = await Subject.findOne({ where: { name: newName } });
+    if (inUse) {
+      return res.status(400).json({ message: "Disciplina já está em uso!" });
+    }
+
+    await Subject.update(
+      {name: newName,},
+      {where: {name:oldName}}                   
+    );
+    res.status(200).json({ message: "Disciplina foi alterada!" });
+  } catch (error) {
+      console.error("❌ Erro ao alterar disciplina:", error);
+      res.status(500).json({ message: "Erro ao alterar disciplina", error: error.message });
+  }
+}
+
--- routes/subjectRoutes.js
+++ routes/subjectRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addSubject, listSubject } from "../controllers/subjectController.js";
+import { addSubject, listSubject, deleteSubject, patchSubject } from "../controllers/subjectController.js";
 import { authenticateToken, verifyAdmin } from "../middlewares/authMiddlewares.js";
 
 const subjectRouter = express.Router();
@@ -48,6 +48,67 @@ subjectRouter.get("/subject", authenticateToken, listSubject);
  */
 subjectRouter.post("/subject", authenticateToken, verifyAdmin, addSubject);
 
+/**
+ * @swagger
+ * /subject:
+ *   delete:
+ *     summary: Deleta uma disciplina pelo nome
+ *     tags: [Disciplina]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: Matemática
+ *     responses:
+ *       200:
+ *         description: Disciplina deletada
+ *       404:
+ *         description: Disciplina não existe
+ */
+subjectRouter.delete("/subject", authenticateToken, verifyAdmin, deleteSubject);
+
+/**
+ * @swagger
+ * /subject:
+ *   patch:
+ *     summary: Altera uma disciplina pelo nome
+ *     tags: [Disciplina]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - oldName
+ *               - newName
+ *             properties:
+ *               oldName:
+ *                 type: string
+ *                 example: Matemática
+ *               newName:
+ *                 type: string
+ *                 example: Português
+ *     responses:
+ *       200:
+ *         description: Disciplina alterada
+ *       404:
+ *         description: Disciplina não existe
+ *       400:
+ *         description: Nome da disciplina já está em uso
+ */
+subjectRouter.patch("/subject", authenticateToken, verifyAdmin, patchSubject);
 
 
 
