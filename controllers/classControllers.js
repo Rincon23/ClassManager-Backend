99d67d4f43412f2f8f1f7dba6b17245b5e6d6b45
Enzo Rincon <enzorincon2003@gmail.com>
2025-10-23 23:17:27 -0300

    Adicionando funcionalidades para deletar e atualizar aulas, incluindo validações e mensagens de erro apropriadas.

--- controllers/classControllers.js
+++ controllers/classControllers.js
@@ -2,6 +2,15 @@ import { Class } from "../models/class.js";
 import { Teacher } from "../models/teacher.js";
 import { Subject } from "../models/subject.js";
 
+export const listClass = async (req, res) => {
+  try {
+    const Classes = await Class.findAll();
+    res.status(200).json(Classes);
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao listar aulas", error });
+  }
+};
+
 export const addClass = async (req, res) => {
   try {
     const {date , timeSlot, teacher, subject} = req.body;
@@ -41,11 +50,48 @@ export const addClass = async (req, res) => {
 }
 }
 
-export const listClass = async (req, res) => {
+export const deleteClass = async (req, res) => {
   try {
-    const Classes = await Class.findAll();
-    res.status(200).json(Classes);
+    const {date , timeSlot} = req.body;
+
+    //Veriifiica se existe alguma aula marcada para esta data
+    const classFound = await Class.findOne({ where: { date, timeSlot } });
+
+    if (!classFound) {
+      return res.status(404).json({ message: "Data não está prevista para ter aula!" });
+    }
+
+    await Class.destroy({ where: { date, timeSlot } });
+    res.status(200).json({ message: "Aula foi removida!" });
   } catch (error) {
-    res.status(500).json({ message: "Erro ao listar aulas", error });
+      console.error("❌ Erro ao deletar Aula:", error);
+      res.status(500).json({ message: "Erro ao deletar Aula", error: error.message });
   }
-};
+}
+
+export const patchClass = async (req, res) => {
+  try {
+    const { oldDate, oldTimeSlot, newDate, newTimeSlot, teacher, subject } = req.body;
+
+    //Verifica se existe a data antiga
+    const oldClassFound = await Class.findOne({ where: { date:oldDate, timeSlot:oldTimeSlot } });
+    if (!oldClassFound) {
+      return res.status(404).json({ message: "Data e hora que está tentando alterar não está prevista para ter aula!" });
+    }
+
+    //Verifica se a data nova está em uso a data
+    const oldClassInUse = await Class.findOne({ where: { date:newDate, timeSlot:newTimeSlot } });
+    if (oldClassInUse) {
+      return res.status(400).json({ message: "Data e hora já está em uso!" });
+    }
+
+    await Class.update(
+      {date: newName, timeSlot: newTimeSlot, teacher, subject},
+      {where: { id: oldClassFound.id }}
+    );
+    res.status(200).json({ message: "Aula foi alterada!" });
+  } catch (error) {
+      console.error("❌ Erro ao alterar disciplina:", error);
+      res.status(500).json({ message: "Erro ao alterar disciplina", error: error.message });
+  }
+}
--- routes/classRoutes.js
+++ routes/classRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addClass, listClass } from "../controllers/classControllers.js";
+import { addClass, listClass, deleteClass, patchClass } from "../controllers/classControllers.js";
 import { authenticateToken, verifyAdmin } from "../middlewares/authMiddlewares.js";
 
 const classRouter = express.Router();
@@ -9,7 +9,7 @@ const classRouter = express.Router();
  * /class:
  *   get:
  *     summary: Lista todos as aulas
- *     tags: [Aulas]
+ *     tags: [Aula]
  *     security:
  *       - bearerAuth: []
  *     responses:
@@ -23,7 +23,7 @@ classRouter.get("/class", authenticateToken, listClass);
  * /class:
  *   post:
  *     summary: Adiciona uma nova aula
- *     tags: [Aulas]
+ *     tags: [Aula]
  *     security:
  *       - bearerAuth: []
  *     requestBody:
@@ -62,5 +62,82 @@ classRouter.get("/class", authenticateToken, listClass);
  *         description: O professor (Nome do professor) não leciona a disciplina (Nome da disciplina). 
  */
 classRouter.post("/class", authenticateToken, verifyAdmin, addClass);
+
+/**
+ * @swagger
+ * /class:
+ *   delete:
+ *     summary: Deleta uma aula pela data e o timeSlot
+ *     tags: [Aula]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - date
+ *               - timeSlot
+ *             properties:
+ *               date:
+ *                 type: dateonly
+ *                 example: 2025-10-21
+ *               timeSlot:
+ *                 type: ENUM
+ *                 example: 19:10 - 20:00
+ *     responses:
+ *       200:
+ *         description: Aula foi removida
+ *       404:
+ *         description: Data não está prevista para ter aula
+ */
+classRouter.delete("/class", authenticateToken, verifyAdmin, deleteClass);
+
+/**
+ * @swagger
+ * /class:
+ *   patch:
+ *     summary: Altera uma Aula pela data e o timeSlot
+ *     tags: [Aula]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - oldDate
+ *               - oldTimeSlot
+ *               - newDate
+ *               - newTimeSlot
+ *               - teacher
+ *               - subject
+ *             properties:
+ *               oldDate:
+ *                 type: dateonly
+ *                 example: 2025-10-21
+ *               oldTimeSlot:
+ *                 type: ENUM
+ *                 example: 19:10 - 20:00
+ *               newDate:
+ *                 type: dateonly
+ *                 example: 2025-10-22
+ *               newTimeSlot:
+ *                 type: ENUM
+ *                 example: 20:00 - 20:50
+ *     responses:
+ *       200:
+ *         description: Aula foi alterada
+ *       404:
+ *         description: Data e hora que está tentando alterar não está prevista para ter aula
+ *       400:
+ *         description: Data e hora já está em uso
+ */
+classRouter.patch("/class", authenticateToken, verifyAdmin, patchClass);
+
 
 export default classRouter;
